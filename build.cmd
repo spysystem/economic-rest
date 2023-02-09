392c44e9edcd45be7215714eef6cbc62900695f4
Thomas Hansen <thomasphansen@users.noreply.github.com>
2023-02-09 11:14:43 +0100

    First commit

---
+++ build.cmd
@@ -0,0 +1,15 @@
+del /s /q .openapi-generator docs lib test > nul 2>&1
+
+docker pull spysystem/openapi-generator:spy_branch
+
+docker run^
+    --rm^
+    -v "%cd%":/local^
+    spysystem/openapi-generator:spy_branch^
+    generate^
+    --generator-name php^
+    --config /local/config.yaml^
+    --input-spec /local/e-conomic.openapi.yaml^
+    --output /local
+
+git add . > nul 2>&1
---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+rm -rf .openapi-generator docs lib test > /dev/null 2>&1
+
+docker pull spysystem/openapi-generator:spy_branch
+
+docker run \
+    --user $(id -u):$(id -g) \
+    --rm \
+    -v ${PWD}:/local \
+    spysystem/openapi-generator:spy_branch \
+    generate \
+    --generator-name php \
+    --config /local/config.yaml \
+    --input-spec /local/e-conomic.openapi.yaml \
+    --output /local
+
+git add . > /dev/null 2>&1
